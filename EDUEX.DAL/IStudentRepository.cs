4ca2320a44e687bed8f7489aacf0ef25f7acb192
Bohdan Yatsyshyn <bohdan.yatsyshyn@emburse.com>
2021-04-14 13:05:03 +0300

    part

---
+++ EDUEX.DAL/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EDUEX.DAL
+{
+    public interface IStudentRepository : IBaseRepository
+    {
+
+    }
+}
---
+++ EDUEX.DAL/StudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using EDUEX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDUEX.DAL
+{
+    public class StudentRepository : BaseRepository, IStudentRepository
+    {
+        public StudentRepository(Func<IEduExDbContext> getDbContext) : base(getDbContext)
+        {
+        }
+
+
+    }
+}
