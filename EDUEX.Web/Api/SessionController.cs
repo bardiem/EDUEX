3887c9d40e23075c47974076176dc6d2304b9aeb
7Woker7 <83217918+7Woker7@users.noreply.github.com>
2021-05-18 13:35:35 +0300

    Update SessionController.cs
    щось типу цього?

--- EDUEX.Web/Api/SessionController.cs
+++ EDUEX.Web/Api/SessionController.cs
@@ -10,7 +10,7 @@ using System.Net;
 namespace EDUEX.Web.Api
 {
     [Route("api/session")]
-    [Authorize]
+    [Authorize(Roles = "admin, user")]
     [ApiController]
     public class SessionController : ControllerBase
     {
@@ -22,7 +22,7 @@ namespace EDUEX.Web.Api
             this.sessionBL = sessionBL;
             this.mapper = mapper;
         }
-        [Authorize(Roles ="admin")]
+        
         [HttpGet]
         [Produces("application/json")]
         [ProducesResponseType(typeof(List<SessionDto>), (int)HttpStatusCode.OK)]
@@ -33,7 +33,6 @@ namespace EDUEX.Web.Api
             return Ok(result);
         }
 
-        [Authorize(Roles = "admin")]
         [HttpGet("{id:int}")]
         [Produces("application/json")]
         [ProducesResponseType(typeof(SessionDto), (int)HttpStatusCode.OK)]
@@ -44,7 +43,6 @@ namespace EDUEX.Web.Api
             return Ok(result);
         }
 
-        [Authorize(Roles = "admin")]
         [HttpPost]
         [Produces("application/json")]
         [ProducesResponseType(typeof(SessionDto), (int)HttpStatusCode.OK)]
@@ -55,7 +53,6 @@ namespace EDUEX.Web.Api
             return Ok(session);
         }
 
-        [Authorize(Roles = "admin")]
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] Session session)
         {
@@ -64,7 +61,6 @@ namespace EDUEX.Web.Api
             return Ok(updateSession);
         }
 
-        [Authorize(Roles = "admin")]
         [HttpDelete("{id}")]
         public IActionResult DeleteSession(int id)
         {
