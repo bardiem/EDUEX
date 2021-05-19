b208432f8a36abcb8bc031426b40f35ff5c2e634
bardi <9bardi@gmail.com>
2021-05-19 11:26:19 +0300

    added newbar to router and a&a for role

--- EDUEX.Client/src/app/default/default.module.ts
+++ EDUEX.Client/src/app/default/default.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TestComponent } from './test/test.component';
 import { routes } from './default.router';
 import { IndexComponent } from './index/index.component';
+import { NewTestComponent } from './new-test/new-test.component';
 
 @NgModule({
   declarations: [
     IndexComponent,
-    TestComponent
+    TestComponent,
+    NewTestComponent
     ],
   imports: [
     CommonModule,
--- EDUEX.Client/src/app/default/default.router.ts
+++ EDUEX.Client/src/app/default/default.router.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { IndexComponent } from './index/index.component';
+import { NewTestComponent } from './new-test/new-test.component';
 
 
 export const router: Routes = [
     { path: '', component : TestComponent },
     { path: 'test', component : IndexComponent  },
+    { path: 'newTest', component: NewTestComponent}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forChild(router);
