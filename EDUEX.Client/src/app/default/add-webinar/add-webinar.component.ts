f96e1947b49c3543e26d9dc39f040a0b50edc83a
bardi <9bardi@gmail.com>
2021-05-28 19:49:25 +0300

    fixed main mistakes

--- EDUEX.Client/src/app/default/add-webinar/add-webinar.component.ts
+++ EDUEX.Client/src/app/default/add-webinar/add-webinar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
 
 @Component({
   selector: 'app-add-webinar',
@@ -8,18 +7,21 @@ import { Injectable } from "@angular/core";
   styleUrls: ['./add-webinar.component.scss']
 })
 
-@Injectable({providedIn : "root"})
 export class AddWebinarComponent {
+  private _url: string = '/api/webinar'; 
+
+  selectedDifficulty: number = 0;
+
   onSubmit(data){
     console.warn(data)
-    this.http.post(this._url,data)
-    .subscribe((result)=>{console.warn(result)
+    this.http.post(this._url, data)
+      .subscribe((result)=>{console.warn(result)
       
     })
   }
   lev=1;
     constructor(private http: HttpClient) {}
-    private _url: string = '/api/webinar'; 
+    
 }
 
 
--- EDUEX.Client/src/app/default/add-webinar/session/session.component.ts
+++ EDUEX.Client/src/app/default/add-webinar/session/session.component.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+
 @Component({
   selector: 'app-session',
   templateUrl: './session.component.html',
   styleUrls: ['./session.component.scss']
 })
 
-  @Injectable({providedIn : "root"})
-  export class SessionComponent{
-    Submit(dat){
-      console.warn(dat)
-      this.http.post(this._url,dat)
+
+export class SessionComponent{
+  private selectedDuration: number = 0;
+  private _url: string = '/api/session';
+
+  Submit(dat){
+    console.warn(dat)
+    this.http.post(this._url, dat)
       .subscribe((res)=>{console.warn(res)
-  
-      })
-       
-    }
-    
-         constructor(private http: HttpClient) {}
-      private _url: string = '/api/session'; 
+
+    })
+      
   }
+  
+        constructor(private http: HttpClient) {}
+      
+}
