a80898bce7e0225bd897f75ed35b246e312cf613
7Woker7 <83217918+7Woker7@users.noreply.github.com>
2021-05-28 16:36:56 +0300

    add webinar

---
+++ EDUEX.Client/src/app/default/add-webinar/add-webinar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-webinar',
+  templateUrl: './add-webinar.component.html',
+  styleUrls: ['./add-webinar.component.scss']
+})
+export class AddWebinarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
